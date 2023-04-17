16cbf2f13e66b2154226e5d9ccd3646fb51285b6
Advance <advance@AdvancedeMacBook-Pro.local>
2023-04-17 20:08:10 +0800

    u

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ let root: any = null
 function render(props: any) {
   const { container } = props;
   root = ReactDOM.createRoot( 
-    container ? container.querySelector('#one-stop-sub-react') : document.getElementById('one-stop-sub-react')
+    container ? container.querySelector('#micro-app') : document.getElementById('micro-app')
   );
   root.render(
     <React.StrictMode>
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,12 +2,12 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../views/index";
 
 const routerOptions = {
-  basename: window.__POWERED_BY_QIANKUN__ ? "/one-stop-sub-react" : "/",
+  basename: window.__POWERED_BY_QIANKUN__ ? "/reactSubApp" : "/",
 };
 
 // create router with basename '/app'
 const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
+  { path: "/index", element: <Home /> },
 ], routerOptions);
 
 export default router;
